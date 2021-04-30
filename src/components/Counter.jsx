8cef0e08ac27b5f59b7f2fd7ae82a9293f9196e5
edelmutic <edelmutic@gmail.com>
2021-04-30 12:56:11 +0300

    add store and update counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
+import counter from '../store/counter';
+
 
 const Counter = () => {
     return (
-        <div style={{ margin: '20px' }}>
-            <Button variant="success" >Plus</Button>{' '}
-            <Button variant="danger">Minus</Button>{' '}
+        <div style={{ margin: '20px', textAlign: "center" }}>
+            <div style={{ fontSize: '1.5rem' }}> {"Count =" + counter.count}</div>
+            <Button variant="success" onClick={() => counter.increment()}>Plus</Button>{' '}
+            <Button variant="danger" onClick={() => counter.decrement()}>Minus</Button>{' '}
         </div>
     )
 }
